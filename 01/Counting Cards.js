c6bf9158d95b6c10f94dfd7e0fe17bbe017e6a96
nelli04 <nelli_ivanova04@mail.ru>
2022-11-23 17:05:31 +0300

    js

---
+++ 01/Counting Cards.js
@@ -0,0 +1,16 @@
+switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
