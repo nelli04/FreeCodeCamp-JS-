16a0b8256491061051c56e3f7f76d5f97736d68e
nelli04 <nelli_ivanova04@mail.ru>
2022-11-24 17:08:01 +0300

    js

---
+++ 01/Manipulating Complex Objects.js
@@ -0,0 +1,27 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+       
+      
+      "gold": true
+    },
+      
+       {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        'YouTube', 'Spotify'
+      ]
+      
+       }
+  ];
+  
+  
